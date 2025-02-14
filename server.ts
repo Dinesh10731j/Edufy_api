f96b269e93694a4f7301d0cd3c864271e0f27992
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-02-14 11:46:02 +0545

    Add MongoDB connection and error handling to server setup

--- server.ts
+++ server.ts
@@ -1,14 +1,26 @@
-import express,{Request,Response} from "express";
+import express,{Request,Response,NextFunction} from "express";
+import { HttpError} from "http-errors";
+import createHttpError from "http-errors";
 import Configuration from "./src/config/config";
-const {Port} = Configuration
+import connectDB from "./src/config/db";
+const {Port,env} = Configuration
 const app = express();
-
-
-app.get("/",(req:Request,res:Response)=>{
-res.json({message:"Hellow world I am server",statusCode:200});
+app.use((err:HttpError,req:Request,res:Response,next:NextFunction):any=>{
+    const statusCode = err.statusCode || 500;
+    return res.status(statusCode).json({message:err.message,errorStack:env==='development' ? err.stack :''});
 
 });
 
-app.listen(Port,()=>{
+
+
+app.get("/",(req:Request,res:Response,next:NextFunction)=>{
+const error = createHttpError(400,"Something went wrong");
+throw error;
+res.json({message:"Hello world I am server",statusCode:200})
+
+});
+
+app.listen(Port,async ()=>{
+    await connectDB();
     console.log(`Listening to port ${Port}`);
 })
