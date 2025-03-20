0880c8529429cc852346c0d1ef2f1374865d6383
Dinesh10731j <www.dineshtmg1122@gmail.com>
2025-03-20 17:01:49 +0545

    Add Cloudinary integration for image uploads and update configuration

--- dist/config/config.js
+++ dist/config/config.js
@@ -10,7 +10,10 @@ const Configuration = {
     Port: (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 1080,
     Mongo_Url: process.env.MONGO_URL,
     env: process.env.NODE_ENV,
-    Jwt_Secret: process.env.JWT_SECRET
+    Jwt_Secret: process.env.JWT_SECRET,
+    Cloudinary_Name: process.env.CLOUDINARY_NAME,
+    Cloudinary_Api_Key: process.env.CLOUDINARY_KEY,
+    Cloudinary_Api_Secret: process.env.CLOUDINARY_SECRET,
 };
 Object.freeze(Configuration);
 exports.default = Configuration;
--- dist/course/course.controller.js
+++ dist/course/course.controller.js
@@ -12,9 +12,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createCourse = void 0;
+exports.uploadImage = exports.createCourse = void 0;
 const http_errors_1 = __importDefault(require("http-errors"));
 const course_model_1 = __importDefault(require("./course.model"));
+const cloudinary_1 = __importDefault(require("../config/cloudinary"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const createCourse = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const coursePrice = 200;
     const { title, hashtags, blocks } = req.body;
@@ -41,3 +44,23 @@ const createCourse = (req, res, next) => __awaiter(void 0, void 0, void 0, funct
     }
 });
 exports.createCourse = createCourse;
+const uploadImage = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!req.file) {
+        res.status(400).json({ message: "No image uploaded." });
+        return;
+    }
+    const filePath = path_1.default.resolve(__dirname, "../../uploads", req.file.filename);
+    try {
+        const result = yield cloudinary_1.default.uploader.upload(filePath, {
+            folder: "course_images",
+            public_id: req.file.filename.split(".")[0],
+        });
+        fs_1.default.unlinkSync(filePath);
+        res.status(200).json({ imageUrl: result.secure_url });
+    }
+    catch (error) {
+        console.error("Cloudinary upload error:", error);
+        next((0, http_errors_1.default)(500, "Image upload failed"));
+    }
+});
+exports.uploadImage = uploadImage;
